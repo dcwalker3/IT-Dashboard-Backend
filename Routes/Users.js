0f142a8ce802ac58e47a2ac345a0cabceb2e510f
Dakota Walker <dwalker29@wvstateu.edu>
2022-03-09 20:45:13 -0500

    Update Users.js

--- Routes/Users.js
+++ Routes/Users.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send(req.query.email);
+
+router.post('/', (req, res) => {
+    console.log(req.body);
+    res.status(200).send('Hello World')
 })
 
 module.exports = router;
